5ac376c7ad9a8fa1a65ea3cb746a0552e40fd4ea
Abdullrahmen <abdullrahmen2003@gmail.com>
2023-09-11 15:04:38 +0000

    Add and fix some tasks

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new = []
-    for i in range(len(my_list)):
-        new[i] = my_list[i]
+    new = my_list.copy()
     if (idx < 0 or idx >= len(my_list)):
         return new
     new[idx] = element
