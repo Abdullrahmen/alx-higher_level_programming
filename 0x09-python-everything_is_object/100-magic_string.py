e0f0fd03a9a8f9a1b657eb5f242ba3082f3a37c2
Abdullrahmen <abdullrahmen2003@gmail.com>
2023-10-03 11:30:58 +0000

    Add and fix some tasks

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-N = 0
 def magic_string():
-    N += 1
-    print("BestSchool" * N)
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
