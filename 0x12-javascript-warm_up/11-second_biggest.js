a25d9c5198eca2a134916c7a39330bf4d3512e9c
Abdullrahmen <abdullrahmen2003@gmail.com>
2024-02-12 19:53:01 +0000

    Add and fix some tasks

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,24 +1,8 @@
 #!/usr/bin/node
-const argv = process.argv;
-if (argv.length === 2 || argv.length === 3) {
-  console.log(0);
+if (process.argv.length <= 3) {
+  console.log('0');
 } else {
-  const big = [];
-  if (Number(argv[2]) > Number(argv[3])) {
-    big[0] = Number(argv[2]);
-    big[1] = Number(argv[3]);
-  } else {
-    big[0] = Number(argv[3]);
-    big[1] = Number(argv[2]);
-  }
-  for (let i = 2; i < argv.length - 2; ++i) {
-    const j = Number(argv[i]);
-    if (j > big[0]) {
-      big[1] = big[0];
-      big[0] = j;
-    } else if (j > big[1]) {
-      big[1] = j;
-    }
-  }
-  console.log(big[1]);
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
