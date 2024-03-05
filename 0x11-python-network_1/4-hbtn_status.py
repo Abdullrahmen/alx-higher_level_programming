8b679910ca3b62fa7256e87fa7910fdbb67ec2c9
Abdullrahmen <abdullrahmen2003@gmail.com>
2024-03-05 18:29:42 +0000

    Add and fix some tasks

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+"""initializate"""
 import requests
 
 
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+def status():
+    """a Python script that fetches https://intranet.hbtn.io/status"""
+
+    data_req = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:\n\t- type: {}\n\t- content: {}".
+          format(type(data_req.text), data_req.text))
+
+
+if __name__ == '__main__':
+    status()
